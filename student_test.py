524accef3c33ebe03742a2b36ac1dd54f108508f
Diego <diegoaer06@gmail.com>
2018-11-28 10:37:26 -0600

    Test for student to answer quiz

--- student_test.py
+++ student_test.py
@@ -1,6 +1,7 @@
 import pytest
 
 from student import Student
+from teacher import Teacher
 
 
 class TestStudent:
@@ -18,3 +19,26 @@ class TestStudent:
         student.add_class('math')
         student.add_class('science')
         assert student.has_class('math') and student.has_class('science')
+
+    def test_student_answer_quiz(self):
+        """Test that a student can answer a quiz"""
+        teacher = Teacher()
+        questions = {
+            '1 + 1?': {
+                '1': False,
+                '2': True,
+                '3': False
+            },
+            'name?': {
+                'my name': True,
+                'your name': True,
+                'no name': False
+            }
+        }
+        teacher.add_class('math')
+        teacher.create_quiz('math', 'quiz1', questions)
+        student = Student()
+        student.add_class('math')
+        teacher.assign_quiz(student, 'math', 'quiz1')
+        assert student.submit_answer('math', 'quiz1', 0, 0) and student.submit_answer(
+            'math', 'quiz1', 0, [1, 2])
