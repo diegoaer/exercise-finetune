f83fa40180e3a3cf7d68b9f6662eee7eaba6b254
Diego <diegoaer06@gmail.com>
2018-11-28 10:46:30 -0600

    Code for answer submission

--- student.py
+++ student.py
@@ -10,3 +10,13 @@ class Student(Person):
             self.quizzes["%s_%s" % (quiz['class_name'], quiz['quiz_name'])] = quiz
             return True
         return False
+
+    def submit_answer(self, class_name, quiz_name, question, answer):
+        if class_name in self.classes:
+            quiz = self.quizzes["%s_%s" % (class_name, quiz_name)]
+            if quiz:
+                if not 'response' in quiz:
+                    quiz['response'] = {}
+                quiz['response']['question'] = answer
+                return True
+        return False
