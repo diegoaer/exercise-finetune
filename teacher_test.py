0ded68ed615c0d1081005cbd70c9b9d74b098c15
Diego <diegoaer06@gmail.com>
2018-11-28 09:41:03 -0600

    Initial teacher quizzes tests

--- teacher_test.py
+++ teacher_test.py
@@ -35,3 +35,92 @@ class TestTeacher:
         student = Student()
         student.add_class('science')
         assert not teacher.teaches(student)
+
+    def test_teacher_create_quiz(self):
+        """Tests if a teacher can create a quiz"""
+        teacher = Teacher()
+        teacher.add_class('math')
+        questions = {
+            '1 + 1?': {
+                '1': False,
+                '2': True,
+                '3': False
+            },
+            'name?': {
+                'my name': True,
+                'your name': True,
+                'no name': False
+            }
+        }
+        teacher.create_quiz('math', 'quiz1', questions)
+        assert len(teacher.get_quizzes()) == 1
+
+    def test_not_teacher_create_quiz(self):
+        """Tests if a teacher cant create a quiz without right answers"""
+        teacher = Teacher()
+        teacher.add_class('math')
+        questions = {'1 + 1?': {'1': False, '2': False, '3': False}}
+        assert teacher.create_quiz('math', 'quiz1', questions)
+
+    def test_teacher_create_quizzes(self):
+        """Tests if a teacher can create multiple quizzes"""
+        teacher = Teacher()
+        teacher.add_class('math')
+        teacher.add_class('science')
+        questions = {
+            '1 + 1?': {
+                '1': False,
+                '2': True,
+                '3': False
+            },
+            'name?': {
+                'my name': True,
+                'your name': True,
+                'no name': False
+            }
+        }
+        teacher.create_quiz('math', 'quiz1', questions)
+        questions = {'1 + 1?': {'1': False, '2': True, '3': False}}
+        teacher.create_quiz('science', 'quiz1', questions)
+        assert len(teacher.get_quizzes()) == 2
+
+    def test_teacher_assign_quiz(self):
+        """Tests if a teacher can assign a quiz to a student"""
+        teacher = Teacher()
+        questions = {
+            '1 + 1?': {
+                '1': False,
+                '2': True,
+                '3': False
+            },
+            'name?': {
+                'my name': True,
+                'your name': True,
+                'no name': False
+            }
+        }
+        teacher.add_class('math')
+        teacher.create_quiz('math', 'quiz1', questions)
+        student = Student()
+        student.add_class('math')
+        assert teacher.assign_quiz(student, 'math', 'quiz1')
+
+    def test_not_teacher_assign_quiz(self):
+        """Tests if a teacher can't assign a uuiz to a student that is not in his class"""
+        teacher = Teacher()
+        questions = {
+            '1 + 1?': {
+                '1': False,
+                '2': True,
+                '3': False
+            },
+            'name?': {
+                'my name': True,
+                'your name': True,
+                'no name': False
+            }
+        }
+        teacher.add_class('math')
+        teacher.create_quiz('math', 'quiz1', questions)
+        student = Student()
+        assert teacher.assign_quiz(student, 'math', 'quiz1')
